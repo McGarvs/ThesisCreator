32dd2dc429227edfb57925459775ffd5de5bb2e0
McGarvs <47725552+McGarvs@users.noreply.github.com>
2024-07-08 09:17:12 -0700

    ESLint & Node Packages

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    jquery: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jquery/recommended'
+  ],
+  rules: {
+    // Add any additional rules specific to your project
+  }
+};
